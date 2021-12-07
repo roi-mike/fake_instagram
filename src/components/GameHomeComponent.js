792f01d71dc69addfb899a1e0d9837266509dfff
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-12-07 20:09:01 +0100

    react ship

--- src/components/GameHomeComponent.js
+++ src/components/GameHomeComponent.js
@@ -1,30 +1,127 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef } from "react";
 
 import "../styles/gameHomeComponentStyle.css";
 
 export default function GameHomeComponent() {
-    
-    const canvasRef = useRef(null);
-    var [state, setstate] = useState(50)
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const FPS = 30; //frame per second
+    const FRICTION = 0.7;//friction coefficient of space (0 = no friction, 1 = lots of friction)
+    const SHIP_SIZE = 10; //ship height in pixels
+    const SHIP_THRUST = 5; // acceleration of the ship in pixels per second
+    const TURN_SPEED = 360; //turn speed in degrees per second
 
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    var ship = {
+      x: canvas.width / 2,
+      y: canvas.height / 2,
+      r: SHIP_SIZE / 2, //rayon
+      a: (90 / 180) * Math.PI, //angle of the ship convert to radians
+      rot: 0,
+      thrusting: false,
+      thrust: {
+        x: 0,
+        y: 0,
+      },
+    };
+
+    window.addEventListener("keydown", keyDown);
+    window.addEventListener("keyup", keyUp);
+
+    function keyUp(/* @type {KeyBoardEvent} */ ev) {
+      switch (ev.keyCode) {
+        case 37: //left arrow (stop rotating left)
+          ship.rot = 0;
+          break;
+        case 38: //up arrow (stop thrusting)
+          ship.thrusting = false;
+          break;
+        case 39: //right arrow (stop rotating right)
+          ship.rot = 0;
+          break;
+        default:
+          break;
+      }
+    }
+
+    function keyDown(/* @type {KeyBoardEvent} */ ev) {
+      switch (ev.keyCode) {
+        case 37: //left arrow (rotate ship left)
+          ship.rot = ((TURN_SPEED / 180) * Math.PI) / FPS;
+          break;
+        case 38: //up arrow (thrusr the ship forward)
+          ship.thrusting = true;
+          console.log("KEYDOWN");
+          break;
+        case 39: //right arrow (rotate ship left)
+          ship.rot = ((-TURN_SPEED / 180) * Math.PI) / FPS;
+          break;
+        default:
+          break;
+      }
+    }
+
+    window.setInterval(function () {
+      //draw space
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle= "green"
-      ctx.fillRect(canvas.width/2,125, 20, 20)
-    },[]);
 
-    window.addEventListener('keydown', (event) => {
-        /*Droite*/
-        if(event.keyCode === 39){
-            setstate(state += 1)
-        /*GAUCHE*/
-        }else if(event.keyCode === 37){
-            setstate(state -= 1)
-        }
-      });
+      if (ship.thrusting) {
+        ship.thrust.x += SHIP_THRUST * Math.cos(ship.a)/ FPS;
+        ship.thrust.y -= SHIP_THRUST * Math.sin(ship.a)/ FPS;
+      }else{ 
+        ship.thrust.x -= FRICTION * ship.thrust.x / FPS;
+        ship.thrust.y -= FRICTION * ship.thrust.y / FPS;
+      }
+
+      //draw ship
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = SHIP_SIZE / 20;
+      ctx.beginPath();
+      ctx.moveTo(
+        //nose of the ship
+        ship.x + (4 / 3) * ship.r * Math.cos(ship.a),
+        ship.y - (4 / 3) * ship.r * Math.sin(ship.a)
+      );
+      ctx.lineTo(
+        //rear left
+        ship.x - ship.r * ((2 / 3) * Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * ((2 / 3) * Math.sin(ship.a) - Math.cos(ship.a))
+      );
+      ctx.lineTo(
+        //rear right
+        ship.x - ship.r * ((2 / 3) * Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * ((2 / 3) * Math.sin(ship.a) + Math.cos(ship.a))
+      );
+      ctx.closePath();
+      ctx.stroke();
+      //rotate ship
+      ship.a += ship.rot;
+      //move the ship
+      ship.x += ship.thrust.x;
+      ship.y += ship.thrust.y;
+
+      //handle edge of screen
+      if(ship.x < 0 - ship.r){
+        ship.x = canvas.width + ship.r;
+      }else if(ship.x > canvas.width + ship.r){
+        ship.x = 0 - ship.r;
+      }
+
+      if(ship.y < 0 - ship.r){
+        ship.y = canvas.height + ship.r;
+      }else if(ship.y > canvas.height + ship.r){
+        ship.y = 0 - ship.r;
+      }
+
+      //centre dot
+      ctx.fillStyle = "red";
+      ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2);
+    }, 1000 / FPS);
+  }, []);
 
   return (
     <div className="gamehome">
--- src/pages/IndexHome.js
+++ src/pages/IndexHome.js
@@ -4,81 +4,92 @@ import BtnConnectionComponent from "../components/btnConnectionComponent";
 
 import "../styles/indexHomeStyle.css";
 import GameHomeComponent from "../components/GameHomeComponent";
+import { Component } from "react";
 
-export default function IndexHome() {
-    
-  return (
-    <div className="container_home">
-      {/*CONTAINER*/}
-      <div className="container_center_home">
-        {/*CONTAINER CENTER*/}
-        <div className="container_presentation">
-          {/*PRESENTATION*/}
-          <div className="container_img_presentation">
-            <div className="container_center_img_presentation">
-              <GameHomeComponent />
-            </div>
-          </div>
+export default class IndexHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-          <div className="container_authenticate">
-            {/*CONTAINER AUTHENTICATE*/}
-            <div className="container_logo_authenticate">
-              <ImgComponent />
-            </div>
-            <div className="container_login_authenticate">
-              <InputComponent />
-              <InputComponent />
-              <BtnConnectionComponent />
-              <p>OU</p>
-              <p>Se connecter avec Facebook</p>
-              <p>Mot de passer oublié</p>
-            </div>
-            <div className="presentation_other_proposal">
-              <p>
-                Vous avez pas de compte ? <span>Inscrivez-vous</span>
-              </p>
-            </div>
-            <div className="presentation_other_proposal_donwload_app">
-              <p>Télécharger l'application</p>
-              <a href="www.google.com">
-                <span> Android</span>
-              </a>
-              <a href="www.apple.com">
-                <span> Apple</span>
-              </a>
-            </div>
-          </div>
-        </div>
+//   componontDidMount() {}
 
-        <div>
-          {/*FOOTER*/}
-          <div>
-            <span>Meta</span>
-            <span>À propos</span>
-            <span>Blog</span>
-            <span>Emplois</span>
-            <span>Aide</span>
-            <span>API</span>
-            <span>Confidencialité</span>
-            <span>Condition</span>
-            <span>Comptes principaux</span>
-            <span>HashTag</span>
-            <span>Lieu</span>
-            <span>Instagram</span>
-            <span>Life</span>
+//   componentWillUnMount() {}
+
+  render() {
+    return (
+      <div className="container_home">
+        {/*CONTAINER*/}
+        <div className="container_center_home">
+          {/*CONTAINER CENTER*/}
+          <div className="container_presentation">
+            {/*PRESENTATION*/}
+            <div className="container_img_presentation">
+              <div className="container_center_img_presentation">
+                <GameHomeComponent />
+              </div>
+            </div>
+
+            <div className="container_authenticate">
+              {/*CONTAINER AUTHENTICATE*/}
+              <div className="container_logo_authenticate">
+                <ImgComponent />
+              </div>
+              <div className="container_login_authenticate">
+                <InputComponent />
+                <InputComponent />
+                <BtnConnectionComponent />
+                <p>OU</p>
+                <p>Se connecter avec Facebook</p>
+                <p>Mot de passer oublié</p>
+              </div>
+              <div className="presentation_other_proposal">
+                <p>
+                  Vous avez pas de compte ? <span>Inscrivez-vous</span>
+                </p>
+              </div>
+              <div className="presentation_other_proposal_donwload_app">
+                <p>Télécharger l'application</p>
+                <a href="www.google.com">
+                  <span> Android</span>
+                </a>
+                <a href="www.apple.com">
+                  <span> Apple</span>
+                </a>
+              </div>
+            </div>
           </div>
 
           <div>
-            <span>Beauté</span>
-            <span>Danse</span>
-            <span>Fitness</span>
-            <span>Alimentation et boissons</span>
-            <span>Maison et jadin</span>
-            <span>Musique</span>
-            <span>Art visuel</span>
+            {/*FOOTER*/}
+            <div>
+              <span>Meta</span>
+              <span>À propos</span>
+              <span>Blog</span>
+              <span>Emplois</span>
+              <span>Aide</span>
+              <span>API</span>
+              <span>Confidencialité</span>
+              <span>Condition</span>
+              <span>Comptes principaux</span>
+              <span>HashTag</span>
+              <span>Lieu</span>
+              <span>Instagram</span>
+              <span>Life</span>
+            </div>
+
+            <div>
+              <span>Beauté</span>
+              <span>Danse</span>
+              <span>Fitness</span>
+              <span>Alimentation et boissons</span>
+              <span>Maison et jadin</span>
+              <span>Musique</span>
+              <span>Art visuel</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
